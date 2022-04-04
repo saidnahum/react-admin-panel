3fa66185e704a24331bdea72b5532121c4ae090b
Said Montes Carballo <70162272+saidnahum@users.noreply.github.com>
2022-04-04 16:19:39 +0000

    Prueba desde Replit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
 	return (
 		<div className="App">
-			Admin Panel - By Said
+			Admin Panel - By Said - Modificado desde Replit Ok
+      <p className='text-5xl'>Lamadev</p>
 		</div>
 	);
 }
